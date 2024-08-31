383c2c7b153356c33747a46da48aaef823e05425
Dimka <godslim1992@gmail.com>
2024-08-31 17:30:06 +0300

    init

---
+++ FileName.cpp
@@ -0,0 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <windows.h>
+using namespace std;
+
+int main() {
+	SetConsoleCP(1251);	SetConsoleOutputCP(1251);
+	srand(time(NULL));
+
+	return 0;
+}
