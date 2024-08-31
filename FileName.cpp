d64f5637dfbd3c1dca6f7a150bf0614688e5491e
Dimka <godslim1992@gmail.com>
2024-08-31 19:10:30 +0300

    Сравнение строк

--- FileName.cpp
+++ FileName.cpp
@@ -7,15 +7,21 @@ using namespace std;
 int main() {
 	SetConsoleCP(1251);	SetConsoleOutputCP(1251);
 	srand(time(NULL));
-	string str;
-	getline(cin, str);
-	cout << "Созданная строка: " << str << endl;
+	string str1, str2;
+	getline(cin, str1);
+	cout << "Созданная строка: " << str1 << endl;
 
-	cout << "Вывод отдельных индексов: " << str[0] << " " << str[2] << " " << str[4] << endl;
+	cout << "Вывод отдельных индексов: " << str1[0] << " " << str1[2] << " " << str1[4] << endl;
 	
-	cout << "Length '" << str << "' = " << str.size() << endl;
-	for (int i = 0; i < str.size(); i++) {
-		cout << "[" << i << "]:\t" << str[i] << endl;
+	cout << "Length '" << str1 << "' = " << str1.size() << endl;
+	for (int i = 0; i < str1.size(); i++) {
+		cout << "[" << i << "]:\t" << str1[i] << endl;
 	}
+
+	getline(cin, str2);
+	if (str1 == str2) {
+		cout << "Строки равны!";
+	}
+	else cout << "Строки НЕ равны!";
 	return 0;
 }
