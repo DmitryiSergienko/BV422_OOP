ced2bc3b34b93d094a5b291b09b1c9e85d04e6cd
Dimka <godslim1992@gmail.com>
2024-08-31 17:49:58 +0300

    Знакомство со строками

--- FileName.cpp
+++ FileName.cpp
@@ -1,11 +1,14 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include <windows.h>
+#include <string>
 using namespace std;
 
 int main() {
 	SetConsoleCP(1251);	SetConsoleOutputCP(1251);
 	srand(time(NULL));
-
+	string str;
+	getline(cin, str);
+	cout << "Созданная строка: " << str << endl;
 	return 0;
 }
