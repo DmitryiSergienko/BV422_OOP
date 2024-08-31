76a6f3f9dad90f5094dd8e610ca731bd35c09cad
Dimka <godslim1992@gmail.com>
2024-08-31 19:22:49 +0300

    Авторизация

--- FileName.cpp
+++ FileName.cpp
@@ -4,24 +4,26 @@
 #include <string>
 using namespace std;
 
+const string LOGIN = "Dimka", PASSWORD = "123qweASD";
+
 int main() {
 	SetConsoleCP(1251);	SetConsoleOutputCP(1251);
 	srand(time(NULL));
-	string str1, str2;
-	getline(cin, str1);
-	cout << "Созданная строка: " << str1 << endl;
-
-	cout << "Вывод отдельных индексов: " << str1[0] << " " << str1[2] << " " << str1[4] << endl;
+	string login;
+	cout << "Enter login: "; cin >> login;
+	if (LOGIN == login) {
+		string password;
+		cout << "Enter password: "; cin >> password;
+		if (PASSWORD == password) {
+			cout << "Welcome\n";
+		}
+		else {
+			cout << "Error, password is not correctly!\n";
+		}
+	}
+	else {
+		cout << "Error, '" << login << "' is not exisist\n";
+	}
 	
-	cout << "Length '" << str1 << "' = " << str1.size() << endl;
-	for (int i = 0; i < str1.size(); i++) {
-		cout << "[" << i << "]:\t" << str1[i] << endl;
-	}
-
-	getline(cin, str2);
-	if (str1 == str2) {
-		cout << "Строки равны!";
-	}
-	else cout << "Строки НЕ равны!";
 	return 0;
 }
